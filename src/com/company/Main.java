547d865f493feba9eaba7763b2418b1774cefe7c
maison armani <maisonarmani38@gmail.com>
2018-07-30 15:54:33 +0100

    Update on file removal

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,31 @@
 package com.company;
 
 
+import javax.swing.text.html.parser.Element;
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
+@interface Rampl{
+    String Unicode() default "Maison";
+    float Version() default 1.0F;
+}
+
 public class Main {
-    private enum Mirrors{
-        CLEAR, DEAD, ALIVE
-    }
-
     public static void main(String[] args) {
-
+        Classic c = new Classic();
+        c.showAnnotation();
         CyclicBarrierDemo demo = new CyclicBarrierDemo();
         demo.runSimulation(5, 3);
     }
 }
 
+
+@Rampl(Unicode = "UTF-8", Version = 0.4F)
+private class Classic{
+    public  void showAnnotation(){
+        Annotation annotation =  (Annotation) this.getClass().getAnnotation(Rampl.class);
+        System.out.println(annotation);
+    }
+}
 
